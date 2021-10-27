864233182dcd8221c25661b7b2a7149e162d3af7
vanessabonis <vanessa1302@gmail.com>
2021-10-27 02:54:49 -0300

    velocidade snake

--- snakeGame.js
+++ snakeGame.js
@@ -1,2 +1,28 @@
 const gameboard = document.getElementById('game-board');
 
+const SNAKE_SPEED = 5;
+var lastTimeRender = 0;
+
+//currentTime -> milliseconds
+function main(currentTime){
+
+    window.requestAnimationFrame(main) 
+    const secondsSinceLastRender = (currentTime = lastTimeRender)/1000
+
+    //velocidade da cobra
+    if(secondsSinceLastRender < 1/SNAKE_SPEED) return;
+    lastTimeRender = currentTime;
+    update();
+    draw();
+
+}
+
+function update(){
+
+}
+
+function draw(){
+
+}
+
+window.requestAnimationFrame(main) 
